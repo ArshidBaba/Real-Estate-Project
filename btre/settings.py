d8d38f07b471da3a79b2f594060176bbea2ec1a9
ArshidBaba <arshid.aslam@insisiv.com>
2020-01-03 22:21:15 +0530

    Updated Settings

--- btre/settings.py
+++ btre/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -153,4 +154,15 @@ EMAIL_HOST_USER = 'arshidbaba93@gmail.com'
 EMAIL_HOST_PASSWORD = 'qkirhvoertifwrtz'
 EMAIL_PORT = 587
 
-django_heroku.settings(locals())
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+
+# django_heroku.settings(locals())
